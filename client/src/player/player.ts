15fd4a865624fce09929fa6bf12e6e3544d06c24
Oleg Zhmelev <losdayver@yandex.ru>
2024-11-03 11:37:19 +0300

    плеер обернут в класс

--- client/src/player/player.ts
+++ client/src/player/player.ts
@@ -1,112 +1,152 @@
 import { iconsPath } from "../routes.js";
 
-const placeholders = document.getElementsByClassName("post-player");
-
-let nowPlayingPlayPauseButton: JQuery<HTMLButtonElement> = null;
-let nowPlaying = false;
+const placeholders = document.getElementsByClassName(
+  "music-player"
+) as HTMLCollectionOf<HTMLDivElement>;
 
 const initPlayers = () => {
-  for (let p of placeholders) {
-    const songName = p.getAttribute("songName");
-    const audioPath = p.getAttribute("audioPath");
-    const thumbnailPath = p.getAttribute("thumbnailPath");
+  for (const p of placeholders) {
+    new MusicPlayer(p);
+  }
+};
 
-    const postPlayer = $(p);
+class MusicPlayer {
+  static lastActivePlayer: MusicPlayer = null;
+  isPlaying = false;
 
+  songName: string;
+  audioPath: string;
+  thumbnailPath: string;
+
+  postPlayer: HTMLDivElement;
+  seekBar: HTMLProgressElement;
+  playPauseButton: HTMLButtonElement;
+  skipForwardButton: HTMLButtonElement;
+  skipBackwardButton: HTMLButtonElement;
+  audio: HTMLAudioElement;
+  timestamp: HTMLParagraphElement;
+
+  play = () => {
+    if (MusicPlayer.lastActivePlayer) MusicPlayer.lastActivePlayer.pause();
+    this.audio.play();
+    $($(this.playPauseButton).children()[0]).attr(
+      "src",
+      `${iconsPath}pause.svg`
+    );
+    MusicPlayer.lastActivePlayer = this;
+    this.isPlaying = true;
+  };
+
+  pause = () => {
+    this.audio.pause();
+    $($(this.playPauseButton).children()[0]).attr(
+      "src",
+      `${iconsPath}play.svg`
+    );
+    MusicPlayer.lastActivePlayer = this;
+    this.isPlaying = false;
+  };
+
+  toggle = () => {
+    if (MusicPlayer.lastActivePlayer == this && this.isPlaying) this.pause();
+    else this.play();
+  };
+
+  getTimestamp = () => {
+    const getFromSeconds = (duration: number) => {
+      let min = +Math.floor(duration / 60)
+        .toString()
+        .padStart(2, "0");
+      let sec = Math.floor(duration - min * 60)
+        .toString()
+        .padStart(2, "0");
+      return `${min}:${sec}`;
+    };
+    const duration = this.audio.duration ?? 0;
+    return `${getFromSeconds(
+      duration * ((this.seekBar.value ?? 0) / 100)
+    )} / ${getFromSeconds(duration)}`;
+  };
+
+  initLogic = () => {
+    this.timestamp.innerText = this.getTimestamp();
+
+    this.playPauseButton.addEventListener("click", this.toggle);
+
+    this.skipBackwardButton.addEventListener(
+      "click",
+      () => (this.audio.currentTime -= 10)
+    );
+
+    this.skipForwardButton.addEventListener(
+      "click",
+      () => (this.audio.currentTime += 10)
+    );
+
+    this.audio.addEventListener("timeupdate", () => {
+      this.seekBar.value = (this.audio.currentTime / this.audio.duration) * 100;
+      this.timestamp.innerText = this.getTimestamp();
+    });
+
+    this.seekBar.addEventListener("input", () => {
+      this.audio.currentTime = (this.audio.duration * this.seekBar.value) / 100;
+      this.timestamp.innerText = this.getTimestamp();
+    });
+  };
+
+  constructor(playerPlaceholder: HTMLDivElement) {
+    // getting div attributes
+    this.songName = playerPlaceholder.getAttribute("songName");
+    this.audioPath = playerPlaceholder.getAttribute("audioPath");
+    this.thumbnailPath = playerPlaceholder.getAttribute("thumbnailPath");
+
+    // building and rendering player based on attributes
+    const postPlayer = $(playerPlaceholder);
     const coverImage = $(
-      `<img class="post-player-cover" src="${thumbnailPath}">`
+      `<img class="music-player-cover" src="${this.thumbnailPath}">`
     );
     postPlayer.append(coverImage);
-    const controlsDiv = $('<div class="post-player-controls"></div>');
+    const controlsDiv = $('<div class="music-player-controls"></div>');
     const seekBar = $<HTMLProgressElement>(
-      '<input class="post-player-slider" type="range" value="0">'
+      '<input class="music-player-slider" type="range" value="0">'
     );
     controlsDiv.append(seekBar);
-    const title = $(`<h3 class="post-player-title">${songName}</h3>`);
+    const title = $(`<h3 class="music-player-title">${this.songName}</h3>`);
     controlsDiv.append(title);
-    const timestamp = $('<p class="post-player-timestamp"></p>');
+    const timestamp = $<HTMLParagraphElement>(
+      '<p class="music-player-timestamp">загрузка</p>'
+    );
     controlsDiv.append(timestamp);
-    const buttonsDiv = $('<div class="post-player-controls-buttons"></div>');
-    const skipBackwardButton = $(
-      `<button class="post-player-controls-buttons-back"><img src="${iconsPath}skip-backward-btn.svg" alt="backward"></button>`
+    const buttonsDiv = $('<div class="music-player-controls-buttons"></div>');
+    const skipBackwardButton = $<HTMLButtonElement>(
+      `<button class="music-player-controls-buttons-back"><img src="${iconsPath}skip-backward-btn.svg" alt="backward"></button>`
     );
     buttonsDiv.append(skipBackwardButton);
     const playPauseButton = $<HTMLButtonElement>(
-      `<button class="post-player-controls-buttons-play"><img src="${iconsPath}play.svg" alt="play"></button>`
+      `<button class="music-player-controls-buttons-play"><img src="${iconsPath}play.svg" alt="play"></button>`
     );
     buttonsDiv.append(playPauseButton);
-    const skipForwardButton = $(
-      `<button class="post-player-controls-buttons-forward"><img src="${iconsPath}skip-forward-btn.svg" alt="forward"></button>`
+    const skipForwardButton = $<HTMLButtonElement>(
+      `<button class="music-player-controls-buttons-forward"><img src="${iconsPath}skip-forward-btn.svg" alt="forward"></button>`
     );
     buttonsDiv.append(skipForwardButton);
     controlsDiv.append(buttonsDiv);
     const audio = $<HTMLAudioElement>(
-      `<audio><source src="${audioPath}" type="audio/mpeg"></audio>`
+      `<audio><source src="${this.audioPath}" type="audio/mpeg"></audio>`
     );
     controlsDiv.append(audio);
     postPlayer.append(controlsDiv);
 
-    window.addEventListener("load", () => {
-      const getTimestamp = () => {
-        const getFromSeconds = (duration: number) => {
-          let min = +Math.floor(duration / 60)
-            .toString()
-            .padStart(2, "0");
-          let sec = Math.floor(duration - min * 60)
-            .toString()
-            .padStart(2, "0");
-          return `${min}:${sec}`;
-        };
-        const duration = audio[0].duration ?? 0;
-        return `${getFromSeconds(
-          duration * ((seekBar[0].value ?? 0) / 100)
-        )} / ${getFromSeconds(duration)}`;
-      };
+    this.postPlayer = postPlayer[0];
+    this.seekBar = seekBar[0];
+    this.playPauseButton = playPauseButton[0];
+    this.skipForwardButton = skipForwardButton[0];
+    this.skipBackwardButton = skipBackwardButton[0];
+    this.audio = audio[0];
+    this.timestamp = timestamp[0];
 
-      timestamp[0].innerText = getTimestamp();
-      let isPlaying = false;
-
-      playPauseButton[0].addEventListener("click", function () {
-        if (isPlaying) {
-          audio[0].pause();
-          nowPlaying = false;
-          $(playPauseButton.children()[0]).attr("src", `${iconsPath}play.svg`);
-        } else {
-          if (
-            nowPlayingPlayPauseButton &&
-            nowPlaying &&
-            nowPlayingPlayPauseButton != playPauseButton
-          )
-            nowPlayingPlayPauseButton.click();
-          nowPlayingPlayPauseButton = playPauseButton;
-          audio[0].play();
-          nowPlaying = true;
-          $(playPauseButton.children()[0]).attr("src", `${iconsPath}pause.svg`);
-        }
-        isPlaying = !isPlaying;
-      });
-
-      skipBackwardButton[0].addEventListener(
-        "click",
-        () => (audio[0].currentTime -= 10)
-      );
-
-      skipForwardButton[0].addEventListener(
-        "click",
-        () => (audio[0].currentTime += 10)
-      );
-
-      audio[0].addEventListener("timeupdate", () => {
-        seekBar[0].value = (audio[0].currentTime / audio[0].duration) * 100;
-        timestamp[0].innerText = getTimestamp();
-      });
-
-      seekBar[0].addEventListener("input", () => {
-        audio[0].currentTime = (audio[0].duration * seekBar[0].value) / 100;
-        timestamp[0].innerText = getTimestamp();
-      });
-    });
+    audio[0].addEventListener("canplay", this.initLogic);
   }
-};
+}
 
-initPlayers();
+window.addEventListener("load", initPlayers);
